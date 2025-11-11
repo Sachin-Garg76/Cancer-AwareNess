3e6ccfc1bcf97ab67ca4b48273d2b86668ed9ff4
Sachin Garg <s32314800@gmail.com>
2025-11-11 11:33:47 +0530

    Create Home component with Navbar and sections

---
+++ src/Components/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './Navbar'
+import MinorNavbar from './MinorNavbar'
+import HeroSection from './HeroSection'
+import About from './Testimonal'
+const Home = () => {
+  return (
+    <>
+     <Navbar/>
+     <MinorNavbar/>
+     <HeroSection/> 
+     <About/> 
+    </>
+  )
+}
+
+export default Home
