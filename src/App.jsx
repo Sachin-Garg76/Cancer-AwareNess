1ea4f9a838d0090034c4470a9ae1e32ccc2a9156
Sachin Garg <s32314800@gmail.com>
2025-11-11 11:32:48 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/js/bootstrap.bundle.js";
+import Home from './Components/Home';
+import "./App.css"
+function App() {
+
+  return (
+    <div className='bg-dark'>
+      <Home/>
+    </div>
+  )
+}
+
+export default App
