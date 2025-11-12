35d8e5d9ab9d34533076c411ec1e2c9d94ee7bf2
Sachin Garg <s32314800@gmail.com>
2025-11-12 22:24:27 +0530

    adding testimonal

--- src/Components/Testimonal.jsx
+++ src/Components/Testimonal.jsx
@@ -1,10 +1,45 @@
+
 import React from 'react'
 
 const About = () => {
   return (
     <>
       <div id="About">
-        <h2></h2>
+        <h2 className='fs-1'>About Cancer Awareness</h2>
+        <p className='fs-5'>Cancer awareness helps people understand the importance of <br />
+        prevention, screening, and treatment.... </p>
+      </div>
+      <div id="Testiomonals">
+        <div className="entities">
+            <img src="" alt="" className="testimonalimg" />
+            <p>
+                I am happy to share <br /> my experience with you <br />
+              and I do donate for this and feel good.. <br />
+            </p>
+        </div>
+        <div className="entities">
+            <img src="" alt="" className="testimonalimg" />
+            <p>
+                I am happy to share <br /> my experience with you <br />
+              and I do donate for this and feel good.. <br />
+            </p>
+        </div>
+        <div className="entities">
+            <img src="" alt="" className="testimonalimg" /> 
+            <p>
+              I am happy to share <br /> my experience with you <br />
+              and I do donate for this and feel good.. <br />
+            </p>
+            <br />
+            
+        </div>
+        <div className="entities">
+            <img src="" alt="" className="testimonalimg" />
+            <p>
+              I am happy to share <br /> my experience with you <br />
+              and I do donate for this and feel good.. <br />
+            </p>
+        </div>
       </div>
     </>
   )
