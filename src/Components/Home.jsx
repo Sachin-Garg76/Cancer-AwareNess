92b191cec79800fb7e5e9975f714155c6836a5b7
Sachin Garg <s32314800@gmail.com>
2025-11-12 22:25:29 +0530

    Add Footer component

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from './Navbar'
 import MinorNavbar from './MinorNavbar'
 import HeroSection from './HeroSection'
 import About from './Testimonal'
+import Footer from './Footer'
 const Home = () => {
   return (
     <>
@@ -10,8 +11,10 @@ const Home = () => {
      <MinorNavbar/>
      <HeroSection/> 
      <About/> 
+     <Footer/> 
     </>
   )
 }
 
 export default Home
+
