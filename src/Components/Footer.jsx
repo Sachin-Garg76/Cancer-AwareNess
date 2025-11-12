9ca31097c5a8526d48e659d745d51cfc8e511c61
Sachin Garg <s32314800@gmail.com>
2025-11-12 22:26:53 +0530

    Add Footer component for Cancer Awareness

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+     <div className="bg-light w-100">
+        <h5 className="text-dark text-center m-3 fs-2">
+            Cancer Awareness @2025   
+        </h5>    
+    </div> 
+    </>
+  )
+}
+
+export default Footer
