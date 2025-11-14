799e4998bc7f92030149120a7ea6de119ca3808a
Sachin Garg <s32314800@gmail.com>
2025-11-14 18:14:34 +0530

    Add developer name to Footer component

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,7 +6,10 @@ const Footer = () => {
      <div className="bg-light w-100">
         <h5 className="text-dark text-center m-3 fs-2">
             Cancer Awareness @2025   
-        </h5>    
+        </h5> 
+        <h5 className="text-dark text-center m-3 fs-2">
+            Sachin @Developer   
+        </h5>   
     </div> 
     </>
   )
